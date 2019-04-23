001d1060107f34d7462491c477588c9485c7c20e
RMCoder198 <rsmawari198@gmail.com>
2019-04-23 13:11:46 +0530

    chnage

--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/api',words);
 app.use(express.static(__dirname ));
-
+app.use(express.static('./images'))
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('../build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
   });
 }
 
